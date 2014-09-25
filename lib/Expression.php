af811cb451b376ee0e85fb1643c0d3c61dc02bb1
frwrd drwrf <hi@drwrf.com>
2014-09-24 23:54:49 -0400

    Move common operators to functions alone.

--- lib/Expression.php
+++ lib/Expression.php
@@ -19,29 +19,59 @@ function expr($expr, $binds = [])
     return new Expr\Expression($expr, $binds);
 }
 
-function eq($value)
-{
-    return new Expr\Eq($value);
-}
-
-function in($value)
-{
-    return new Expr\In($value);
-}
-
 function not($value)
 {
-    return new Expr\Eq($value);
+    return new Expr\Not($value);
+}
+
+function gt($value)
+{
+    return new Expr\Expression('%s > ?', [$value]);
+}
+
+function gte($value)
+{
+    return new Expr\Expression('%s >= ?', [$value]);
+}
+
+function after($value)
+{
+    return gt($value);
+}
+
+function lt($value)
+{
+    return new Expr\Expression('%s < ?', [$value]);
+}
+
+function lte($value)
+{
+    return new Expr\Expression('%s <= ?', [$value]);
+}
+
+function before($value)
+{
+    return lt($value);
+}
+
+function like($value)
+{
+    return new Expr\Expression('%s LIKE ?', [$value]);
+}
+
+function between($value1, $value2)
+{
+    return new Expr\Expression('%s BETWEEN ? AND ?', [$value1, $value2]);
 }
 
 function blank()
 {
-    return new Expr\Blank();
+    return new Expr\Expression('%s IS NULL OR NOT %s', []);
 }
 
 function null()
 {
-    return new Expr\Null();
+    return new Expr\Expression('%s IS NULL', []);
 }
 
 function columnize($column, $source)
