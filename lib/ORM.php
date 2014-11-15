daa3cba3ef14d63d0d482d2c3efdc28a9e95e112
♥ <hi@drwrf.com>
2014-11-15 16:19:32 -0500

    Namespace all ORM keys.

--- lib/ORM.php
+++ lib/ORM.php
@@ -15,12 +15,12 @@ trait ORM
     public static function usingORM($m)
     {
         // The table that we want to connect the model to.
-        $m['table'] = function($m) {
+        $m['orm:table'] = function($m) {
             return Inflector::tableize($m->getClass());
         };
 
         // The primary key of the model, as an attribute.
-        $m['pk'] = function($m) {
+        $m['orm:pk'] = function($m) {
             foreach ($m['attrs'] as $attr) {
                 if ($attr instanceof Primary) {
                     return $attr;
@@ -31,26 +31,29 @@ trait ORM
         };
 
         // The default foreign key of the model, by name.
-        $m['fk'] = function($m) {
-            return $m['table'] . '_id';
+        $m['orm:fk'] = function($m) {
+            return $m['orm:table'] . '_id';
         };
 
         // The connection the model will use to talk to the database.
-        $m['connection'] = $m->factory(function ($m) {
-            return $m['connection:class']::create($m['credentials']);
+        $m['orm:connection'] = $m->factory(function ($m) {
+            return $m['orm:connection:class']::create($m['orm:credentials']);
         });
 
         // The class to use for this model's connection.
-        $m['connection:class'] = 'Mismatch\ORM\Connection';
+        $m['orm:connection:class'] = 'Mismatch\ORM\Connection';
 
         // The query builder used for finding and modifying data
-        $m['query'] = $m->factory(function($m) {
-            $query = new $m['query:class']($m['connection'], $m['table'], $m['pk']);
+        $m['orm:query'] = $m->factory(function($m) {
+            $query = new $m['orm:query:class'](
+                $m['orm:connection'],
+                $m['orm:table'],
+                $m['orm:pk']);
 
             return $query;
         });
 
         // The class to use for query building.
-        $m['query:class'] = 'Mismatch\ORM\Query';
+        $m['orm:query:class'] = 'Mismatch\ORM\Query';
     }
 }
